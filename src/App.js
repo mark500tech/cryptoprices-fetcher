c33478d478967abdff97bc89901d9f39bb4db510
mark500tech <mark@500tech.com>
2017-12-22 23:15:42 +0200

    Project initialization

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { createStore } from 'redux';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+          Hello page!
+      </div>
+    );
+  }
+}
+
+export default App;
